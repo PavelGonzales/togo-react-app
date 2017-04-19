058c66e6494e2576a5250a2225b090d5889fdfe0
Pavel Gonzales <develop.gonzales@gmail.com>
2017-04-19 22:06:30 +0300

    Styled modal window

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import preview from './images/preview.jpg';
 import Modal from 'react-modal';
 import './index.css';
+import './modal.css';
 
-const customStyles = {
-  content : {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
+Modal.setAppElement('#root');
 
 class App extends React.Component {
   constructor() {
@@ -24,21 +16,19 @@ class App extends React.Component {
     };
 
     this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
   openModal() {
-    this.setState({modalIsOpen: true});
-  }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.refs.subtitle.style.color = '#f00';
+    this.setState({
+      modalIsOpen: true
+    });
   }
 
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({
+      modalIsOpen: false
+    });
   }
 
   render() {
@@ -48,17 +38,38 @@ class App extends React.Component {
           <span>фильм пойдем</span>
         </h1>
         <img className="card-сinema__preview" src={preview} alt="preview"/>
-        <button className="card-сinema__button" onClick={this.openModal}>Следующий фильм</button>
+        <button className="button" onClick={this.openModal}>Следующий фильм</button>
         <Modal
           isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
-          style={customStyles}
           contentLabel="Example Modal"
+          className="modal"
+          overlayClassName="overlay"
         >
-
-          <h2 ref="subtitle">Инфа о фильме</h2>
-          <button onClick={this.closeModal}>Закрыть</button>
+          <div className="info">
+            <div className="info__poster">
+              <img src={preview} alt="poster"/>
+            </div>
+            <div className="info__content">
+              <h2 className="info__title">Инфа о фильме</h2>
+              <p className="info__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores recusandae inventore pariatur adipisci nesciunt sed incidunt earum aliquam, suscipit illo ut numquam, at molestiae quibusdam ad error, culpa sint non.</p>
+            </div>
+          </div>
+          <div className="cinemas">
+            <h3>Где идет:</h3>
+            <ul>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+            </ul>
+          </div>
+          <button className="button" onClick={this.closeModal}>Закрыть</button>
         </Modal>
       </div>
     );
