4290911d7612690165894a04c7077724a34e0d3c
Pavel Gonzales <develop.gonzales@gmail.com>
2017-04-19 21:11:53 +0300

    Added modal window

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import preview from './images/preview.jpg';
+import Modal from 'react-modal';
 import './index.css';
 
-class App extends Component {
+const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     return (
       <div className="card-сinema">
@@ -11,7 +48,18 @@ class App extends Component {
           <span>фильм пойдем</span>
         </h1>
         <img className="card-сinema__preview" src={preview} alt="preview"/>
-        <button className="card-сinema__button">Следующий фильм</button>
+        <button className="card-сinema__button" onClick={this.openModal}>Следующий фильм</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref="subtitle">Инфа о фильме</h2>
+          <button onClick={this.closeModal}>Закрыть</button>
+        </Modal>
       </div>
     );
   }
