83ea0b1e01ed565b0459d57e202d9ab3f4770882
Pavel Gonzales <develop.gonzales@gmail.com>
2017-04-26 12:54:51 +0300

    added redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import axios from 'axios';
-import preview from './../images/preview.jpg';
+// import axios from 'axios';
+import { connect } from 'react-redux';
 
 import Poster from './Poster';
 import ButtonNext from './Button';
@@ -8,48 +8,48 @@ import ButtonNext from './Button';
 
 import './../styles/index.css';
 
-export class App extends React.Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
       cinemaData: [],
-      poster: preview,
+      // poster: preview,
     };
   }
 
-  componentDidMount() {
-    axios.get(`https://kudago.com/public-api/v1.3/movies/?location=msk&fields=id,title,body_text,poster&page_size=100`)
-      .then((response) => {
-        const cinemaData = this.shuffle(response.data.results);
-        this.setState({
-          cinemaData: cinemaData
-        })
-      })
-      .catch((error) => {
-        console.log('error', error);
-      })
+  // componentDidMount() {
+  //   axios.get(`https://kudago.com/public-api/v1.3/movies/?location=msk&fields=id,title,body_text,poster&page_size=100`)
+  //     .then((response) => {
+  //       const cinemaData = this.shuffle(response.data.results);
+  //       this.setState({
+  //         cinemaData: cinemaData
+  //       })
+  //     })
+  //     .catch((error) => {
+  //       console.log('error', error);
+  //     })
 
-  }
+  // }
 
-  shuffle(arr) {
-    const array = arr;
-    for (let i = array.length; i; i--) { // eslint-disable-line
-      const j = Math.floor(Math.random() * i);
-      [array[i - 1], array[j]] = [array[j], array[i - 1]];
-    }
-    return array;
-  }
+  // shuffle(arr) {
+  //   const array = arr;
+  //   for (let i = array.length; i; i--) { // eslint-disable-line
+  //     const j = Math.floor(Math.random() * i);
+  //     [array[i - 1], array[j]] = [array[j], array[i - 1]];
+  //   }
+  //   return array;
+  // }
 
   render() {
-    console.log('DONE! ', this.state.cinemaData);
+    console.log(this.props.testStore);
     
     return (
       <div className="card-сinema">
         <h1 className="card-сinema__title">На какой<br/>
           <span>фильм пойдем</span>
         </h1>
-        <Poster poster={this.state.poster}/>
+        <Poster poster={this.props.testStore[0].poster}/>
         <ButtonNext cinemaData={this.nextCinema}/>
         {/*<Popup/>*/}
       </div>
@@ -57,4 +57,9 @@ export class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({}),
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import preview from './images/preview.jpg';
 import App from './components/App'
 
+const initialState = [
+  {
+    poster: preview,
+  }
+];
+
+function cinemalist(state = initialState, action) {
+  if (action.type === 'NEXT_CINEMA') {
+    return [
+      ...state,
+      action.payload
+    ];
+  }
+  return state;
+}
+
+const store = createStore(cinemalist);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>   
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
